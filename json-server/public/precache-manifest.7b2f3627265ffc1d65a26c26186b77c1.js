1dd826470c02be8c7e1d564628b6899d2473b53b
oyolah <oyolahmustapha2015@gmail.com>
2020-04-09 20:29:32 +0300

    server side

---
+++ json-server/public/precache-manifest.7b2f3627265ffc1d65a26c26186b77c1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd8ca953bcad5f834c59faaa882010e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd73ca38bc5c037d1e27",
+    "url": "/static/css/2.7f0a60d8.chunk.css"
+  },
+  {
+    "revision": "5665b1d99aff2c26c642",
+    "url": "/static/css/main.667ee8c4.chunk.css"
+  },
+  {
+    "revision": "dd73ca38bc5c037d1e27",
+    "url": "/static/js/2.d6e90be3.chunk.js"
+  },
+  {
+    "revision": "95fe7b47190d8ffb46ace11b00922862",
+    "url": "/static/js/2.d6e90be3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5665b1d99aff2c26c642",
+    "url": "/static/js/main.537a4c34.chunk.js"
+  },
+  {
+    "revision": "7d6ed5c99dace444a7f8",
+    "url": "/static/js/runtime-main.af9eefbc.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
